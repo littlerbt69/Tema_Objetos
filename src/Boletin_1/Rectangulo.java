383c973b32e5b717d9f57dfae16cc2585fb2f29e
roberto <robertocd509@gmail.com>
2023-11-26 17:52:57 +0100

    Cambios dia 26/11

--- src/Boletin_1/Rectangulo.java
+++ src/Boletin_1/Rectangulo.java
@@ -34,13 +34,31 @@ public class Rectangulo {
         }
     }
 
+    // Método para calcular el perímetro
+    public double calcularPerimetro() {
+        return 2 * (longitud + ancho);
+    }
+
+    // Método para calcular el área
+    public double calcularArea() {
+        return longitud * ancho;
+    }
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Introduce longitud del rectangulo: ");
-        double nuevaLondigtud = teclado.nextDouble();
+        Rectangulo rectangulo = new Rectangulo();
+
+        System.out.print("Introduce longitud del rectángulo: ");
+        double nuevaLongitud = teclado.nextDouble();
+        rectangulo.setLongitud(nuevaLongitud);
 
         System.out.print("Ingrese el ancho del rectángulo: ");
         double nuevoAncho = teclado.nextDouble();
+        rectangulo.setAncho(nuevoAncho);
+
+        // Mostrar el área y el perímetro del rectángulo
+        System.out.println("Área del rectángulo: " + rectangulo.calcularArea());
+        System.out.println("Perímetro del rectángulo: " + rectangulo.calcularPerimetro());
     }
 }
