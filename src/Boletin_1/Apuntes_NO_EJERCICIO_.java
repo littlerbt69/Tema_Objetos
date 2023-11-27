c9635fe3a2441343d26d28bff41b928ce4d46f65
roberto <robertocd509@gmail.com>
2023-11-27 12:38:43 +0100

    cambios dia 27/11

---
+++ src/Boletin_1/Apuntes_NO_EJERCICIO_.java
@@ -0,0 +1,9 @@
+package Boletin_1;
+
+public class Apuntes_NO_EJERCICIO_ {
+    /*
+        Las variables static suelen ser constantes comunes para todos los objetos que creemos en la clase, son compartidas para todos los objetos.
+
+        Las variables no static suelen ser variables que son usadas para definir valores propios de los objetos.
+    */
+}
